0166116ee36c018afcf2e5c4a24fe5b908603df5
bbeltran22 <99772609+bbeltran22@users.noreply.github.com>
2022-07-07 10:46:14 -0600

    Edited default city

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App container mt-2">
-      <Weather defaultCity="New York" />
+      <Weather defaultCity={"New York"} />
     </div>
   );
 }
