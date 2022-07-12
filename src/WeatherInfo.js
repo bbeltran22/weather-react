8a9bb5d5a782b7c966ee8d313ff5f58b6e3e0347
bbeltran22 <99772609+bbeltran22@users.noreply.github.com>
2022-07-12 09:38:25 -0600

    Css

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -10,21 +10,22 @@ export default function WeatherInfo(props) {
         <div className="temp col ">
           <Temp fahrenheit={props.info.temp} />
         </div>
-        <div className="col ">
+        <div className="col">
           <span className="icon">
             <WeatherIcon code={props.info.icon} />
           </span>
         </div>
       </div>
       <div className="row mt-2 mb-2">
-        <div className="stat col-3">
-          High: {Math.round(props.info.highTemp)}°F
+        <div className="stat col-4">
+          <span className="high">
+            High: {Math.round(props.info.highTemp)}°F
+          </span>
         </div>
-        <div className="stat col-3">
+        <div className="stat col-4">
           Low: {Math.round(props.info.lowTemp)}°F
         </div>
-        <div className="stat col-3">Humidity: {props.info.humidity}%</div>
-        <div className="stat col-3 mb-2">Air Quality: ?</div>
+        <div className="stat col-4">Humidity: {props.info.humidity}%</div>
       </div>
     </div>
   );
